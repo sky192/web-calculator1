8a325e28fc36acffa3e9d8d72b7be3ed62bd2196
sky192 <samahehloghmani@gmail.com>
2020-09-26 13:07:57 -0400

    First push

---
+++ script.js
@@ -0,0 +1,7 @@
+function add() {
+    var x = parseInt(document.getElementById("first").value);
+    var y = parseInt(document.getElementById("second").value);
+    var z = x + y;
+    document.getElementById("result").value = z;
+
+}
